e4f9bb5de9e1ce3643c40c1d92ee4e1a71846f5a
Quentin <quentin.garnierr@gmail.com>
2025-02-13 09:42:22 +0100

    update exo 1

---
+++ src/main/java/exo1/ContactServices.java
@@ -0,0 +1,5 @@
+package exo1;
+
+public interface ContactServices {
+    void envoiEmail(Contact contact, String message);
+}
---
+++ src/main/java/exo1/ContactsService.java
@@ -0,0 +1,7 @@
+package exo1;
+
+public interface ContactsService {
+    void ajouteContact(Contact contact);
+    void supprimeContact(Contact contact);
+    void afficheContacts();
+}
